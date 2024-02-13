f415d573e2abba16c094f6ff9c607c0cf7469558
Andy <amluck88@gmail.com>
2024-02-13 11:55:42 -0500

    finish up to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function(loc) {
-    for (i = 0; i = loc; i++)
-        
+const fibonacci = function(count) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
+
+};
 
 };
 
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookTitles = [books[0].title, books[1].title];
+    return bookTitles;
 };
 
 // Do not edit below this line
